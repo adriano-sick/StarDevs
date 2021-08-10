eb80885a902437a77f4f5913a7c0f6b4b1a3fcfa
Adriano Siqueira <adriano_siqueira_silva@hotmail.com>
2021-08-10 03:56:05 -0300

    init

--- JS/index.js
+++ JS/index.js
@@ -1,3 +1,5 @@
+/* Adriano Siqueira - 10/8/2021 */
+
 const persons = document.getElementById('persons');
 const starships = document.getElementById('starships');
 const planets = document.getElementById('planets');
